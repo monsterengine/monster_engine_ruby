e61029bcc965b8c1f28267d841ab576884e3aa34
Shogo Otake <shogo.otake@gmail.com>
2019-12-17 17:45:13 +0900

    Impl prefork

--- ext/monster_engine/monster_engine_server.c
+++ ext/monster_engine/monster_engine_server.c
@@ -1,36 +1,69 @@
 #include "monster_engine.h"
 #include <ruby/thread.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+extern const rb_data_type_t rb_plamo_app_type;
+extern VALUE plamo_rb_event_thread(void*);
 
 VALUE rb_cMonsterEngineServer;
+
+static void deallocate(void *monster_engine_server) {
+  monster_engine_server_destroy(monster_engine_server);
+}
+
+const rb_data_type_t rb_monster_engine_server_type = {
+  "Server",
+  {
+    NULL,
+    deallocate,
+    NULL,
+  },
+  NULL,
+  NULL,
+  0,
+};
+
+static VALUE allocate(VALUE klass) {
+  return TypedData_Wrap_Struct(klass, &rb_monster_engine_server_type, NULL);
+}
+
+static VALUE initialize(VALUE self, VALUE rb_plamo_app, VALUE rb_monster_engine_config) {
+  PlamoApp *plamo_app;
+  TypedData_Get_Struct(rb_plamo_app, PlamoApp, &rb_plamo_app_type, plamo_app);
+  MonsterEngineConfig *monster_engine_config;
+  TypedData_Get_Struct(rb_monster_engine_config, MonsterEngineConfig, &rb_monster_engine_config_type, monster_engine_config);
+  DATA_PTR(self) = monster_engine_server_new(plamo_app, monster_engine_config);
+  return self;
+}
 
 typedef struct MonsterEngineRbCallbackSetting {
   PlamoApp *plamo_app;
   MonsterEngineConfig *monster_engine_config;
 } MonsterEngineRbCallbackSetting;
 
-static void* callback(void *value) {
-  MonsterEngineRbCallbackSetting *setting = (MonsterEngineRbCallbackSetting*)value;
-  monster_engine_server_start(setting->plamo_app, setting->monster_engine_config);
+static void* callback(void *monster_engine_server) {
+  monster_engine_server_start(monster_engine_server);
   return NULL;
 }
 
-extern const rb_data_type_t rb_plamo_app_type;
+static VALUE start(VALUE self) {
+  for (int i = 0; i < 3; i++) {
+    pid_t pid = fork();
+    if (pid == 0) {
+      rb_thread_create(plamo_rb_event_thread, NULL);
+      rb_thread_call_without_gvl(callback, DATA_PTR(self), RUBY_UBF_PROCESS, NULL);
+      break;
+    } else if (pid == -1) {
+      break;
+    }
+  }
 
-static VALUE start(VALUE self, VALUE rb_plamo_app, VALUE rb_monster_engine_config) {
-  PlamoApp *plamo_app;
-  TypedData_Get_Struct(rb_plamo_app, PlamoApp, &rb_plamo_app_type, plamo_app);
-
-  MonsterEngineConfig *monster_engine_config;
-  TypedData_Get_Struct(rb_monster_engine_config, MonsterEngineConfig, &rb_monster_engine_config_type, monster_engine_config);
-  MonsterEngineRbCallbackSetting *monster_engine_rb_callback_setting = malloc(sizeof(MonsterEngineRbCallbackSetting));
-  monster_engine_rb_callback_setting->plamo_app = plamo_app;
-  monster_engine_rb_callback_setting->monster_engine_config = monster_engine_config;
-
-  rb_thread_call_without_gvl(callback, monster_engine_rb_callback_setting, RUBY_UBF_PROCESS, NULL);
   return Qnil;
 }
 
 void Init_monster_engine_server(void) {
   rb_cMonsterEngineServer = rb_define_class_under(rb_mMonsterEngine, "Server", rb_cObject);
-  rb_define_singleton_method(rb_cMonsterEngineServer, "start", start, 2);
+  rb_define_method(rb_cMonsterEngineServer, "initialize", initialize, 2);
+  rb_define_method(rb_cMonsterEngineServer, "start", start, 0);
 }
