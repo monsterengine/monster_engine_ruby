ed38811301e3447f595e4ff4bc4f877ceeaec6df
Shogo Otake <shogo.otake@gmail.com>
2019-12-18 15:51:58 +0900

    Add Config#bind and Config#bind=

--- ext/monster_engine/monster_engine_config.c
+++ ext/monster_engine/monster_engine_config.c
@@ -22,13 +22,24 @@ static VALUE allocate(VALUE klass) {
   return TypedData_Wrap_Struct(klass, &rb_monster_engine_config_type, NULL);
 }
 
-static VALUE initialize(VALUE self, VALUE rb_bind, VALUE rb_workers) {
-  DATA_PTR(self) = monster_engine_config_new(StringValueCStr(rb_bind), FIX2UINT(rb_workers));
+static VALUE initialize(VALUE self) {
+  DATA_PTR(self) = monster_engine_config_new();
   return self;
+}
+
+static VALUE get_bind(VALUE self) {
+  return rb_str_new2(monster_engine_config_get_bind(DATA_PTR(self)));
+}
+
+static VALUE set_bind(VALUE self, VALUE rb_bind) {
+  monster_engine_config_set_bind(DATA_PTR(self), StringValueCStr(rb_bind));
+  return Qnil;
 }
 
 void Init_monster_engine_config(void) {
   rb_cMonsterEngineConfig = rb_define_class_under(rb_mMonsterEngine, "Config", rb_cObject);
   rb_define_alloc_func(rb_cMonsterEngineConfig, allocate);
-  rb_define_method(rb_cMonsterEngineConfig, "initialize", initialize, 2);
+  rb_define_method(rb_cMonsterEngineConfig, "initialize", initialize, 0);
+  rb_define_method(rb_cMonsterEngineConfig, "bind", get_bind, 0);
+  rb_define_method(rb_cMonsterEngineConfig, "bind=", set_bind, 1);
 }
